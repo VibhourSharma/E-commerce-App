f1462f87c42aa797862ad984595c4ef926b3e222
Vibhour Sharma <vibhour121@gmail.com>
2023-12-02 03:12:34 +0530

    🚀item card & categories updated

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,14 +3,11 @@ import ItemsCard from "./ItemsCard";
 
 const Categories = () => {
   return (
-    <div className="w-[73%] font-default lowercase">
+    <>
       {data.top_products.map((topCatData) => {
         return (
-          <div>
-            <div
-              key={topCatData.category_id}
-              className="flex items-center justify-between my-4 py-3 bg-[#F2F2F2] rounded-lg"
-            >
+          <div className="w-[73%] font-default" key={topCatData.category_id}>
+            <div className="flex items-center justify-between my-4 py-3 bg-[#F2F2F2] rounded-lg">
               <div className="text-lg flex ml-4 font-bold normal-case">
                 {topCatData.category_name}
                 <div className="ml-3 bg-blue-500 text-[#F2F2F2] rounded-lg text-sm flex items-center justify-center w-7 font-medium">
@@ -21,11 +18,17 @@ const Categories = () => {
                 See All
               </div>
             </div>
-            <ItemsCard products={topCatData.products} />
+            <div className="flex flex-wrap items-center justify-between gap-4">
+              {topCatData.products.map((productData) => {
+                return (
+                  <ItemsCard productData={productData} key={productData.id} />
+                );
+              })}
+            </div>
           </div>
         );
       })}
-    </div>
+    </>
   );
 };
 
--- src/components/ItemsCard.jsx
+++ src/components/ItemsCard.jsx
@@ -1,48 +1,48 @@
 import React from "react";
 
-const ItemsCard = ({ products }) => {
+const ItemsCard = ({ productData }) => {
   const URL =
     "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
+  const totalOff = Math.floor(
+    ((productData.original_cost - productData.base_cost) /
+      productData.original_cost) *
+      100
+  );
   return (
-    <div>
-      {products.map((productsData) => {
-        return (
-          <div className="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
-            <div className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl">
-              <img
-                className="object-fit w-full h-full"
-                src={URL}
-                alt="product image"
-              />
-              <span className="absolute top-0 right-0 m-2 bg-black px-2 text-center text-sm font-medium text-white">
-                24% OFF
-              </span>
-            </div>
-            <div className="mt-4 px-5 pb-5">
-              <div>
-                <h5 className="text-xl tracking-tight text-slate-900">
-                  {productsData.name}
-                </h5>
-              </div>
-              <div>per unit</div>
-              <div className="mt-2 mb-5 flex items-center justify-between">
-                <p>
-                  <span className="text-3xl font-bold text-slate-900">
-                    $449
-                  </span>
-                  <span className="text-sm text-slate-900 line-through">
-                    $699
-                  </span>
-                </p>
-              </div>
-              <div className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
-                Add to cart
-              </div>
-            </div>
-          </div>
-        );
-      })}
+    <div className="relative flex justify-between flex-col overflow-hidden rounded-lg border border-gray-100 bg-white max-w-[18rem] h-[24rem] p-2">
+      <div className="relative flex h-56 overflow-hidden rounded-xl">
+        <img
+          className="object-fit w-full h-full"
+          src={URL}
+          alt="product image"
+        />
+        {totalOff !== 0 ? (
+          <span className="absolute top-0 right-0 m-2 bg-black px-2 text-center text-sm font-medium text-white">
+            {totalOff}% Off
+          </span>
+        ) : (
+          ""
+        )}
+      </div>
+      <div
+        className=" text-base font-medium
+       lowercase tracking-tight whitespace-wrap max-w-[14rem] my-2"
+      >
+        {productData.name}
+      </div>
+      <div>{productData.base_qty}</div>
+      <div className="py-2">
+        <span className="text-xl font-bold text-slate-900">
+          ₹{productData.base_cost}
+        </span>
+        <span className="text-sm text-slate-900 line-through">
+          ₹{productData.original_cost}
+        </span>
+      </div>
+      <div className="flex items-center justify-center rounded-md bg-slate-900 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+        Add to cart
+      </div>
     </div>
   );
 };
