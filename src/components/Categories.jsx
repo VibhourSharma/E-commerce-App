1860e6035a1efcc61899a1d755e6f9cc981e90fe
Vibhour Sharma <vibhour121@gmail.com>
2023-12-01 23:58:35 +0530

    🚀added products inside product categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,17 +1,27 @@
 import data from "../data";
+import ItemsCard from "./ItemsCard";
 
 const Categories = () => {
   return (
     <div className="w-[73%] font-default lowercase">
-      {data.top_categories.map((topCatData) => {
+      {data.top_products.map((topCatData) => {
         return (
-          <div className="flex items-center justify-start my-4 py-3 bg-[#F2F2F2] rounded-lg">
-            <div className="text-lg flex ml-4 font-bold normal-case">
-              {topCatData.name}
-              <div className="ml-3 bg-blue-500 text-[#F2F2F2] rounded-lg text-sm flex items-center justify-center w-7 font-medium">
-                {topCatData.product_count}
+          <div>
+            <div
+              key={topCatData.category_id}
+              className="flex items-center justify-between my-4 py-3 bg-[#F2F2F2] rounded-lg"
+            >
+              <div className="text-lg flex ml-4 font-bold normal-case">
+                {topCatData.category_name}
+                <div className="ml-3 bg-blue-500 text-[#F2F2F2] rounded-lg text-sm flex items-center justify-center w-7 font-medium">
+                  {topCatData.product_count}
+                </div>
+              </div>
+              <div className="capitalize mr-8 text-blue-600 font-medium">
+                See All
               </div>
             </div>
+            <ItemsCard products={topCatData.products} />
           </div>
         );
       })}
--- src/components/ItemsCard.jsx
+++ src/components/ItemsCard.jsx
@@ -1,36 +1,48 @@
 import React from "react";
 
-const ItemsCard = () => {
+const ItemsCard = ({ products }) => {
   const URL =
     "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
   return (
-    <div className="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
-      <div className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl">
-        <img
-          className="object-fit w-full h-full"
-          src={URL}
-          alt="product image"
-        />
-        <span className="absolute top-0 right-0 m-2 bg-black px-2 text-center text-sm font-medium text-white">
-          24% OFF
-        </span>
-      </div>
-      <div className="mt-4 px-5 pb-5">
-        <div>
-          <h5 className="text-xl tracking-tight text-slate-900">Food</h5>
-        </div>
-        <div>per unit</div>
-        <div className="mt-2 mb-5 flex items-center justify-between">
-          <p>
-            <span className="text-3xl font-bold text-slate-900">$449</span>
-            <span className="text-sm text-slate-900 line-through">$699</span>
-          </p>
-        </div>
-        <div className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
-          Add to cart
-        </div>
-      </div>
+    <div>
+      {products.map((productsData) => {
+        return (
+          <div className="relative m-10 flex w-full max-w-xs flex-col overflow-hidden rounded-lg border border-gray-100 bg-white shadow-md">
+            <div className="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl">
+              <img
+                className="object-fit w-full h-full"
+                src={URL}
+                alt="product image"
+              />
+              <span className="absolute top-0 right-0 m-2 bg-black px-2 text-center text-sm font-medium text-white">
+                24% OFF
+              </span>
+            </div>
+            <div className="mt-4 px-5 pb-5">
+              <div>
+                <h5 className="text-xl tracking-tight text-slate-900">
+                  {productsData.name}
+                </h5>
+              </div>
+              <div>per unit</div>
+              <div className="mt-2 mb-5 flex items-center justify-between">
+                <p>
+                  <span className="text-3xl font-bold text-slate-900">
+                    $449
+                  </span>
+                  <span className="text-sm text-slate-900 line-through">
+                    $699
+                  </span>
+                </p>
+              </div>
+              <div className="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+                Add to cart
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 };
