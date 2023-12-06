58049425347801ec2d02f609469830c1e6f5a713
Vibhour Sharma <vibhour121@gmail.com>
2023-12-07 00:05:30 +0530

    🚀added active state in navlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,34 +6,57 @@ const Navbar = () => {
   return (
     <div className="flex w-full h-16 items-center justify-center font-default fixed left-0 top-0 width-[100%]  shadow-sm bg-white z-50">
       <div className="flex w-[73%] items-center justify-around h-full">
-        <NavLink
-          to="/"
-          className={({ isActive }) => `
-        ${isActive ? "text-[#3B82F6]" : "text-gray-400"}`}
-        >
-          <div className="flex flex-col items-center text-gray-400 hover:text-[#3B82F6]">
+        <div>
+          <NavLink
+            to="/"
+            className={({ isActive }) => `
+        ${
+          isActive ? "text-[#3B82F6]" : "text-gray-400"
+        } flex flex-col items-center`}
+          >
             <FiHome className="text-3xl" />
             <span className="text-xs">Home</span>
-          </div>
-        </NavLink>
-        <NavLink to="/categories">
-          <div className="flex flex-col items-center text-gray-400 hover:text-[#3B82F6]">
+          </NavLink>
+        </div>
+
+        <div>
+          <NavLink
+            to="/categories"
+            className={({ isActive }) => `
+        ${
+          isActive ? "text-[#3B82F6]" : "text-gray-400"
+        } flex flex-col items-center`}
+          >
             <BiCategoryAlt className="text-3xl" />
             <span className="text-xs">Categories</span>
-          </div>
-        </NavLink>
-        <NavLink to="/bag">
-          <div className="flex flex-col items-center text-gray-400 hover:text-[#3B82F6]">
+          </NavLink>
+        </div>
+
+        <div>
+          <NavLink
+            to="/bag"
+            className={({ isActive }) => `
+        ${
+          isActive ? "text-[#3B82F6]" : "text-gray-400"
+        } flex flex-col items-center`}
+          >
             <FiShoppingBag className="text-3xl" />
             <span className="text-xs">Bag</span>
-          </div>
-        </NavLink>
-        <NavLink to="/orders">
-          <div className="flex flex-col items-center text-gray-400 hover:text-[#3B82F6]">
+          </NavLink>
+        </div>
+
+        <div>
+          <NavLink
+            to="/orders"
+            className={({ isActive }) => `
+        ${
+          isActive ? "text-[#3B82F6]" : "text-gray-400"
+        } flex flex-col items-center`}
+          >
             <FiList className="text-3xl" />
             <span className="text-xs">Orders</span>
-          </div>
-        </NavLink>
+          </NavLink>
+        </div>
       </div>
     </div>
   );
