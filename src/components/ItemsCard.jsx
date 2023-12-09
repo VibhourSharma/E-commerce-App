4a62349578096c0ea390923b7487d4f611a8ccdd
Vibhour Sharma <vibhour121@gmail.com>
2023-12-09 23:44:00 +0530

    ✅items page created

--- src/components/ItemsCard.jsx
+++ src/components/ItemsCard.jsx
@@ -1,50 +1,35 @@
-const ItemsCard = ({ productData }) => {
+import React from "react";
+
+const ItemPage = () => {
   const URL =
     "https://images.unsplash.com/photo-1546069901-ba9599a7e63c?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
-  const totalOff = Math.floor(
-    ((productData.original_cost - productData.base_cost) /
-      productData.original_cost) *
-      100
-  );
   return (
-    <div className="relative flex justify-between flex-col overflow-hidden rounded-lg border border-gray-100 bg-white w-[16.5rem] h-[24rem] p-2">
-      <div className="relative flex h-56 overflow-hidden rounded-xl">
-        <img
-          className="object-fit w-full h-full"
-          src={URL}
-          alt="product image"
-        />
-        {totalOff !== 0 ? (
-          <span className="absolute top-0 right-0 m-2 bg-orange-500 px-2 text-center text-sm font-medium text-white rounded-md">
-            {totalOff}% Off
-          </span>
-        ) : (
-          ""
-        )}
-      </div>
-      <div
-        className=" text-base font-medium
-       lowercase tracking-tight whitespace-wrap max-w-[14rem] my-2"
-      >
-        {productData.name}
-      </div>
-      <div>{productData.base_qty}</div>
-      <div className="py-2 flex justify-between">
-        <div className="flex items-center justify-center">
-          <span className="text-xl font-bold text-slate-900">
-            ₹{productData.base_cost}
-          </span>
-          <span className="text-sm text-slate-900 line-through">
-            ₹{productData.original_cost}
-          </span>
+    <div className="w-full flex items-center justify-center font-default">
+      <div className="w-[73%] flex">
+        <div className="h-[50%] w-[50%]">
+          <img src={URL} alt="category id" />
         </div>
-        <div className="flex items-center justify-center rounded-md text-blue-700 py-2 px-2 text-center text-sm font-medium border border-blue-700 cursor-pointer">
-          Add to cart
+        <div className="w-[70%] mt-8 ml-12">
+          <div className="flex flex-col">
+            <span className="tracking-wide text-lg">
+              EVEREST TURMERIC (HALDI) 200 GRAM
+            </span>
+            <span className="text-sm mt-1">per unit</span>
+            <span className="text-lg font-bold mt-2">$53</span>
+          </div>
+          <div className="flex justify-between items-center mt-8">
+            <button className="border border-blue-600 p-3.5 text-blue-500 w-[47%] rounded-lg">
+              Add To Bag
+            </button>
+            <button className="bg-blue-600 text-white p-3.5 w-[47%] rounded-lg">
+              Buy Now
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default ItemsCard;
+export default ItemPage;
