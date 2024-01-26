59c7fcb331b28733500ec910b168b6174b3aff58
Vibhour Sharma <vibhour121@gmail.com>
2024-01-26 23:53:52 +0530

    🚀Feature: Add/Remove to/from cart feature added

--- src/pages/Bag.jsx
+++ src/pages/Bag.jsx
@@ -1,69 +1,74 @@
 import Navbar from "../components/Navbar";
 import { MdDeleteForever } from "react-icons/md";
+import { CartContext } from "../context/Cart";
+import { useState, useContext } from "react";
 
 const Bag = () => {
-  const URL =
-    "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?q=80&w=1981&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+  const { cartItems, addToCart, removeFromCart, clearCart, getCartTotal } =
+    useContext(CartContext);
+  console.log(cartItems);
+
   return (
     <>
       <Navbar />
-      <header class="text-center mt-20">
-        <h1 class="text-xl font-bold text-gray-900 sm:text-3xl">Your Cart</h1>
-      </header>
-      <div class="mx-auto max-w-screen-xl">
-        <div class="mt-8 flex items-center justify-between">
-          <ul class="space-y-4 w-[63%]">
-            <li class="flex items-center">
-              <img src={URL} alt="" class="h-32 w-32 rounded object-cover" />
-
-              <div className="ml-4">
-                <h3 class="text-xl text-gray-900">Basic Tee 6-Pack</h3>
-
-                <dl class="mt-0.5 space-y-px text-sm text-gray-600">
-                  <div>
-                    <dt class="inline">Per Unit</dt>
+      <div className="w-full h-full flex items-center justify-center font-default">
+        <div className="mt-20 w-[73%] text-center">
+          {cartItems.length === 0 ? (
+            <span className="text-2xl font-medium">
+              Your cart is currently empty
+            </span>
+          ) : (
+            <span className=" font-medium flex items-center justify-between">
+              <span className="text-xl">
+                Your Cart has a total of {cartItems.length} items
+              </span>
+              <button
+                className="flex items-center justify-around p-2 border-[1px] border-black transition hover:text-red-600 hover:border-red-600 rounded-lg"
+                onClick={() => clearCart()}
+              >
+                <p>Clear Cart</p>
+                <span className="w-6 h-6 flex items-center justify-center">
+                  <MdDeleteForever className="w-full h-full" />
+                </span>
+              </button>
+            </span>
+          )}
+          <div className="flex items-center justify-between">
+            <div>
+              {cartItems.map((presentItem) => (
+                <>
+                  <div className="flex mt-4">
+                    <img
+                      src={presentItem.image}
+                      alt={`Image number: ${presentItem.id}`}
+                      className="w-48 h-48"
+                    />
+                    <div className="flex flex-col items-start ml-4">
+                      <span>{presentItem.name}</span>
+                      <span>{presentItem.base_qty}</span>
+                      <span className="my-4">₹{presentItem.base_cost}</span>
+                      <div className="flex items-center justify-center rounded-md text-blue-700 w-[5rem] h-[2.2rem] text-center text-sm font-medium border border-blue-700 cursor-pointer">
+                        <button
+                          onClick={() => removeFromCart(presentItem)}
+                          className="text-2xl w-[33.33%]"
+                        >
+                          -
+                        </button>
+                        <span className="text-base w-[33.33%]">
+                          {presentItem.quantity}
+                        </span>
+                        <button
+                          onClick={() => addToCart(presentItem)}
+                          className="text-2xl w-[33.33%]"
+                        >
+                          +
+                        </button>
+                      </div>
+                    </div>
                   </div>
-
-                  <div>
-                    <dt class="inline">₹ Price</dt>
-                  </div>
-                </dl>
-              </div>
-
-              <div class="flex flex-1 items-center justify-end gap-2">
-                <button class="text-gray-600 transition hover:text-red-600">
-                  <MdDeleteForever className="w-8 h-8" />
-                </button>
-              </div>
-            </li>
-          </ul>
-
-          <div class="mt-8 flex justify-end border-t border-gray-100 pt-8">
-            <div class="w-screen max-w-sm space-y-4">
-              <dl class="space-y-0.5 text-sm text-gray-700">
-                <div class="flex justify-between">
-                  <dt>Subtotal</dt>
-                  <dd>₹250</dd>
-                </div>
-
-                <div class="flex justify-between">
-                  <dt>Discount</dt>
-                  <dd>₹20</dd>
-                </div>
-
-                <div class="flex justify-between !text-base font-medium">
-                  <dt>Total</dt>
-                  <dd>₹200</dd>
-                </div>
-              </dl>
-              <div class="flex justify-end">
-                <a
-                  href="#"
-                  class="block rounded bg-gray-700 px-5 py-3 text-sm text-gray-100 transition hover:bg-gray-600"
-                >
-                  Place Order
-                </a>
-              </div>
+                </>
+              ))}
+              <div></div>
             </div>
           </div>
         </div>
@@ -73,3 +78,31 @@ const Bag = () => {
 };
 
 export default Bag;
+
+{
+  /* <div class="mt-8 flex justify-end border-t border-gray-100 pt-8">
+  <div class="w-screen max-w-sm space-y-4">
+    <dl class="space-y-0.5 text-sm text-gray-700">
+      <div class="flex justify-between">
+        <dt>Subtotal</dt>
+        <dd>₹250</dd>
+      </div>
+
+      <div class="flex justify-between">
+        <dt>Delivery Charges</dt>
+        <dd>₹30</dd>
+      </div>
+
+      <div class="flex justify-between !text-base font-medium">
+        <dt>Total</dt>
+        <dd>₹200</dd>
+      </div>
+    </dl>
+    <div class="flex justify-end">
+      <button class="block rounded bg-gray-700 px-5 py-3 text-sm text-gray-100 transition hover:bg-gray-600">
+        Place Order
+      </button>
+    </div>
+  </div>
+</div>; */
+}
