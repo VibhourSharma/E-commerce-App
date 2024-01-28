cc0673102ff7e59af1159e809342bb402a225c7e
Vibhour Sharma <vibhour121@gmail.com>
2024-01-28 21:51:26 +0530

    ⚙️Feature: Enter key press search functionality added

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,24 +1,37 @@
-import { IoIosSearch } from "react-icons/io";
-import { useSearchParams, Link } from "react-router-dom";
 import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { IoIosSearch } from "react-icons/io";
 
 const SearchBar = () => {
-  const [searchParams] = useSearchParams();
   const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      navigate(`/search?search_query=${search}`);
+    }
+  };
+
+  const handleSearchButtonClick = () => {
+    navigate(`/search?search_query=${search}`);
+  };
+
   return (
     <div className="flex items-center justify-center mt-20 mb-6 w-[73%]">
       <input
         onChange={(e) => setSearch(e.target.value.toLowerCase())}
+        onKeyPress={handleKeyPress}
         value={search}
         type="text"
         className="bg-[#F2F2F2] p-2 w-full rounded-lg focus:outline-none focus:outline-blue-300"
         placeholder="Search for products..."
       />
-      <Link to={search.trim() === "" ? "/" : `/search?search_query=${search}`}>
-        <button className="flex items-center justify-center w-12 h-10 text-2xl ml-2 rounded-lg bg-[#F2F2F2]">
-          <IoIosSearch />
-        </button>
-      </Link>
+      <button
+        onClick={handleSearchButtonClick}
+        className="flex items-center justify-center w-12 h-10 text-2xl ml-2 rounded-lg bg-[#3B82F6] text-white"
+      >
+        <IoIosSearch />
+      </button>
     </div>
   );
 };
